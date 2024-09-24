ca42c33b6b71a8b5f1aa783594ee4b6b9be5bab9
Deco <122901973+deco775@users.noreply.github.com>
2024-09-24 18:54:21 +0200

    Update admin.py

--- student_manage/admin.py
+++ student_manage/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Student
 
-/*creating a registration for our model*/
+#/*creating a registration for our model*/
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Student._meta.get_fields()]
