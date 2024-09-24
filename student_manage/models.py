0f480adf920545eb19ef3599528acaa3ef87d7ea
Deco <122901973+deco775@users.noreply.github.com>
2024-09-24 18:38:56 +0200

    Student management

--- student_manage/models.py
+++ student_manage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+/*This are courses that students are enrolled*/
 MY_CHOICES = [
         ("CE", 'CE'),
         ("EXTC", 'EXTC'),
@@ -8,13 +9,14 @@ MY_CHOICES = [
         ("IT", 'IT'),
     ]
 
-
+/*creating a model for student inf0*/
 class Student(models.Model):
     fname=models.CharField(max_length=25)
     lname=models.CharField(max_length=25)
     email=models.EmailField(max_length=25)
     phone=models.PositiveIntegerField()
     branch=models.CharField(max_length=50,choices=MY_CHOICES)
-
+        
+/* defining the string function for our model*/
     def __str__(self):
         return self.fname
